c5d7002292b5f54ed849109f21918c078bbbc873
Heston Snodgrass <hsnodgrass3@gmail.com>
2020-06-30 14:04:29 -0700

    Refactored the code to be a bit more sane

--- scrape_cm1000.py
+++ scrape_cm1000.py
@@ -58,8 +58,7 @@ i_uc_codewords = Gauge(
 )
 
 
-def scrape_modem(modem_ip, username, password):
-    # Authenticate and get the status page
+def get_status_page(modem_ip: str, username: str, password: str):
     with requests.Session() as s:
         data = {}
         headers = {
@@ -72,90 +71,139 @@ def scrape_modem(modem_ip, username, password):
         data['login'] = '1'
         token = soup.select_one('input[name="webToken"]')['value']
         data['webToken'] = token
-        payload = urllib.parse.urlencode(data, quote_via=urllib.parse.quote_plus)
-        s.post(f'http://{modem_ip}/goform/GenieLogin', headers=headers, data=payload)
+        payload = urllib.parse.urlencode(data,
+                                         quote_via=urllib.parse.quote_plus)
+        s.post(f'http://{modem_ip}/goform/GenieLogin',
+               headers=headers,
+               data=payload)
         status_page = s.get(f'http://{modem_ip}/DocsisStatus.asp')
     if status_page.status_code == 200:
         total_scrapes.inc()
-        # Scrape data from the status page
-        soup = BeautifulSoup(status_page.content, features='html.parser')
-        ds_table = soup.find('table', id='dsTable').find_all('tr')[1:]
-        us_table = soup.find('table', id='usTable').find_all('tr')[1:]
-        ds_ofdm_table = soup.find('table', id='d31dsTable').find_all('tr')[1:]
-        us_ofdma_table = soup.find('table', id='d31usTable').find_all('tr')[1:]
-        ds_hash = {}
-        ds_hash['channel_type'] = 'bonded'
-        ds_hash['direction'] = 'downstream'
-        us_hash = {}
-        us_hash['channel_type'] = 'bonded'
-        us_hash['direction'] = 'upstream'
-        ds_ofdm_hash = {}
-        ds_ofdm_hash['channel_type'] = 'ofdm'
-        ds_ofdm_hash['direction'] = 'downstream'
-        us_ofdma_hash = {}
-        us_ofdma_hash['channel_type'] = 'ofdma'
-        us_ofdma_hash['direction'] = 'upstream'
-        for each in ds_table:
-            ds_hash[each.contents[0].get_text()] = {
-                'lock_status': each.contents[1].get_text(),
-                'modulation': each.contents[2].get_text(),
-                'channel_id': each.contents[3].get_text(),
-                'frequency': each.contents[4].get_text(),
-                'power': each.contents[5].get_text(),
-                'snr_mer': each.contents[6].get_text(),
-                'unerrored_codewords': each.contents[7].get_text(),
-                'correctable_codewords': each.contents[8].get_text(),
-                'uncorrectable_codewords': each.contents[9].get_text()
-            }
-        for each in us_table:
-            us_hash[each.contents[0].get_text()] = {
-                'lock_status': each.contents[1].get_text(),
-                'modulation': each.contents[2].get_text(),
-                'channel_id': each.contents[3].get_text(),
-                'frequency': each.contents[4].get_text(),
-                'power': each.contents[5].get_text(),
-            }
-        for each in ds_ofdm_table:
-            ds_ofdm_hash[each.contents[0].get_text()] = {
-                'lock_status': each.contents[1].get_text(),
-                'modulation': each.contents[2].get_text(),
-                'channel_id': each.contents[3].get_text(),
-                'frequency': each.contents[4].get_text(),
-                'power': each.contents[5].get_text(),
-                'snr_mer': each.contents[6].get_text(),
-                'active_subcarrier_number_range': each.contents[7].get_text(),
-                'unerrored_codewords': each.contents[8].get_text(),
-                'correctable_codewords': each.contents[9].get_text(),
-                'uncorrectable_codewords': each.contents[10].get_text()
-            }
-        for each in us_ofdma_table:
-            us_ofdma_hash[each.contents[0].get_text()] = {
-                'lock_status': each.contents[1].get_text(),
-                'modulation': each.contents[2].get_text(),
-                'channel_id': each.contents[3].get_text(),
-                'frequency': each.contents[4].get_text(),
-                'power': each.contents[5].get_text()
-            }
-        logging.info('Successfully scraped metrics...')
-        return {
-            'ds_bonded': ds_hash,
-            'us_bonded': us_hash,
-            'ds_ofdm': ds_ofdm_hash,
-            'us_ofdma': us_ofdma_hash
-        }
+        return status_page.content
     else:
-        logging.error('HTTP Error: Failed to scrape metrics, check total_failed_scrapes metric...')
+        logging.error('HTTP Error: Failed to scrape metrics, '
+                      'check total_failed_scrapes metric...')
         total_failed_scrapes.labels(
             http_response_code=status_page.status_code
         ).inc()
-        raise Exception('HTTP Error: Failed to scrape metrics')
+        raise Exception('HTTP Error: Failed to scrape metrics!')
+
+
+def downstream_dict(channel_type, table):
+    out_hash = {
+        'direction': 'downstream',
+        'channel_type': channel_type
+    }
+    for each in table:
+        out_hash[each.contents[0].get_text()] = {
+            'lock_status': each.contents[1].get_text(),
+            'modulation': each.contents[2].get_text(),
+            'channel_id': each.contents[3].get_text(),
+            'frequency': each.contents[4].get_text(),
+            'power': each.contents[5].get_text(),
+            'snr_mer': each.contents[6].get_text(),
+            'unerrored_codewords': each.contents[7].get_text(),
+            'correctable_codewords': each.contents[8].get_text(),
+            'uncorrectable_codewords': each.contents[9].get_text()
+        }
+    return out_hash
+
+
+def upstream_dict(channel_type, table):
+    out_hash = {
+        'direction': 'upstream',
+        'channel_type': channel_type
+    }
+    for each in table:
+        out_hash[each.contents[0].get_text()] = {
+            'lock_status': each.contents[1].get_text(),
+            'modulation': each.contents[2].get_text(),
+            'channel_id': each.contents[3].get_text(),
+            'frequency': each.contents[4].get_text(),
+            'power': each.contents[5].get_text(),
+        }
+    return out_hash
+
+
+def scrape_modem(modem_ip, username, password):
+    # Scrape data from the status page
+    content = get_status_page(modem_ip, username, password)
+    soup = BeautifulSoup(content, features='html.parser')
+    ds_table = soup.find('table', id='dsTable').find_all('tr')[1:]
+    us_table = soup.find('table', id='usTable').find_all('tr')[1:]
+    ds_ofdm_table = soup.find('table', id='d31dsTable').find_all('tr')[1:]
+    us_ofdma_table = soup.find('table', id='d31usTable').find_all('tr')[1:]
+    ds_hash = {}
+    ds_hash['channel_type'] = 'bonded'
+    ds_hash['direction'] = 'downstream'
+    us_hash = {}
+    us_hash['channel_type'] = 'bonded'
+    us_hash['direction'] = 'upstream'
+    ds_ofdm_hash = {}
+    ds_ofdm_hash['channel_type'] = 'ofdm'
+    ds_ofdm_hash['direction'] = 'downstream'
+    us_ofdma_hash = {}
+    us_ofdma_hash['channel_type'] = 'ofdma'
+    us_ofdma_hash['direction'] = 'upstream'
+    for each in ds_table:
+        ds_hash[each.contents[0].get_text()] = {
+            'lock_status': each.contents[1].get_text(),
+            'modulation': each.contents[2].get_text(),
+            'channel_id': each.contents[3].get_text(),
+            'frequency': each.contents[4].get_text(),
+            'power': each.contents[5].get_text(),
+            'snr_mer': each.contents[6].get_text(),
+            'unerrored_codewords': each.contents[7].get_text(),
+            'correctable_codewords': each.contents[8].get_text(),
+            'uncorrectable_codewords': each.contents[9].get_text()
+        }
+    for each in us_table:
+        us_hash[each.contents[0].get_text()] = {
+            'lock_status': each.contents[1].get_text(),
+            'modulation': each.contents[2].get_text(),
+            'channel_id': each.contents[3].get_text(),
+            'frequency': each.contents[4].get_text(),
+            'power': each.contents[5].get_text(),
+        }
+    for each in ds_ofdm_table:
+        ds_ofdm_hash[each.contents[0].get_text()] = {
+            'lock_status': each.contents[1].get_text(),
+            'modulation': each.contents[2].get_text(),
+            'channel_id': each.contents[3].get_text(),
+            'frequency': each.contents[4].get_text(),
+            'power': each.contents[5].get_text(),
+            'snr_mer': each.contents[6].get_text(),
+            'active_subcarrier_number_range': each.contents[7].get_text(),
+            'unerrored_codewords': each.contents[8].get_text(),
+            'correctable_codewords': each.contents[9].get_text(),
+            'uncorrectable_codewords': each.contents[10].get_text()
+        }
+    for each in us_ofdma_table:
+        us_ofdma_hash[each.contents[0].get_text()] = {
+            'lock_status': each.contents[1].get_text(),
+            'modulation': each.contents[2].get_text(),
+            'channel_id': each.contents[3].get_text(),
+            'frequency': each.contents[4].get_text(),
+            'power': each.contents[5].get_text()
+        }
+    logging.info('Successfully scraped metrics...')
+    return {
+        'ds_bonded': ds_hash,
+        'us_bonded': us_hash,
+        'ds_ofdm': ds_ofdm_hash,
+        'us_ofdma': us_ofdma_hash
+    }
 
 
 def set_locked(channel: str, channel_type: str, direction: str, metrics: dict):
     if metrics['lock_status'] == 'Locked':
-        i_locked.labels(channel=channel, channel_type=channel_type, direction=direction).set(1)
+        i_locked.labels(channel=channel,
+                        channel_type=channel_type,
+                        direction=direction).set(1)
     else:
-        i_locked.labels(channel=channel, channel_type=channel_type, direction=direction).set(0)
+        i_locked.labels(channel=channel,
+                        channel_type=channel_type,
+                        direction=direction).set(0)
 
 
 def set_ds_metrics(metrics: dict):
@@ -164,12 +212,36 @@ def set_ds_metrics(metrics: dict):
     for key, val in metrics.items():
         if key != 'channel_type' and key != 'direction':
             set_locked(key, channel_type, direction, val)
-            i_freq.labels(channel=key, channel_type=channel_type, direction=direction).set(float(val['frequency'].replace('Hz', '').strip()))
-            i_power.labels(channel=key, channel_type=channel_type, direction=direction).set(float(val['power'].replace('dBmV', '').strip()))
-            i_snrmer.labels(channel=key, channel_type=channel_type, direction=direction).set(float(val['snr_mer'].replace('dB', '').strip()))
-            i_ue_codewords.labels(channel=key, channel_type=channel_type, direction=direction).set(float(val['unerrored_codewords']))
-            i_co_codewords.labels(channel=key, channel_type=channel_type, direction=direction).set(float(val['correctable_codewords']))
-            i_uc_codewords.labels(channel=key, channel_type=channel_type, direction=direction).set(float(val['correctable_codewords']))
+            i_freq.labels(
+                channel=key,
+                channel_type=channel_type,
+                direction=direction
+            ).set(float(val['frequency'].replace('Hz', '').strip()))
+            i_power.labels(
+                channel=key,
+                channel_type=channel_type,
+                direction=direction
+            ).set(float(val['power'].replace('dBmV', '').strip()))
+            i_snrmer.labels(
+                channel=key,
+                channel_type=channel_type,
+                direction=direction
+            ).set(float(val['snr_mer'].replace('dB', '').strip()))
+            i_ue_codewords.labels(
+                channel=key,
+                channel_type=channel_type,
+                direction=direction
+            ).set(float(val['unerrored_codewords']))
+            i_co_codewords.labels(
+                channel=key,
+                channel_type=channel_type,
+                direction=direction
+            ).set(float(val['correctable_codewords']))
+            i_uc_codewords.labels(
+                channel=key,
+                channel_type=channel_type,
+                direction=direction
+            ).set(float(val['correctable_codewords']))
 
 
 def set_us_metrics(metrics: dict):
@@ -192,29 +264,61 @@ def export_metrics(modem_ip, username, password, interval):
 
 
 if __name__ == '__main__':
+    import os
+    import sys
+    import yaml
     from yaml import Loader
 
     try:
-        import os
-        conf = {
-            'modem_ip': os.getenv('PCM_MODEM_IP', '192.168.100.1'),
-            'username': os.getenv('PCM_USERNAME', 'admin'),
-            'password': os.environ['PCM_PASSWORD'],
-            'export_port': os.getenv('PCM_EXPORT_PORT', 9527),
-            'interval': os.getenv('PCM_INTERVAL', 10)
-        }
-    except KeyError:
-        logging.warning('Environment variables not found, using config file...')
-        with open('/usr/local/prom_cm1000.yaml') as f:
-            conf = yaml.load(f.read())
+        yaml_path = sys.argv[1]
+    except IndexError:
+        yaml_path = '/usr/local/prom_cm1000.yaml'
 
-    start_http_server(int(conf.get('export_port', 9527)))
+    # Default configs
+    dconf = {
+        'modem_ip': '192.168.100.1',
+        'username': 'admin',
+        'password': '',
+        'export_port': 9527,
+        'interval': 10
+    }
+    if os.path.exists(yaml_path):
+        with open(yaml_path) as f:
+            yconf = yaml.load(f.read(), Loader=Loader)
+            dconf = {**dconf, **yconf}
+    else:
+        logging.info(f'YAML config file path {yaml_path} not found'
+                     ', using envvars...')
+    econf = {
+        'modem_ip': os.getenv('PCM_modem_ip', '192.168.100.1'),
+        'username': os.getenv('PCM_username', 'admin'),
+        'password': os.getenv('PCM_password', ''),
+        'export_port': int(os.getenv('PPE_export_port', 9527)),
+        'interval': int(os.getenv('PPE_interval', 10))
+    }
+    conf = {
+        **dconf,
+        **{k: v for (k, v) in econf.items() if v}
+    }
+    if not conf['password']:
+        logging.critical('Password is require! Exiting...')
+        exit(1)
+    # Config with password censored for logs
+    censored_config = {
+        'modem_ip': conf['modem_ip'],
+        'username': conf['username'],
+        'password': '********',
+        'export_port': conf['export_port'],
+        'interval': conf['interval']
+    }
+    start_http_server(conf['export_port'])
+    logging.info(f'Starting CM1000 exporting with config: {censored_config}')
     while True:
         try:
-            export_metrics(conf.get('modem_ip', '192.168.100.1'),
-                           conf.get('username', 'admin'),
+            export_metrics(conf['modem_ip'],
+                           conf['username'],
                            conf['password'],
-                           int(conf.get('interval', 10)))
+                           conf['interval'])
         except Exception as exc:
             logging.error(exc)
             logging.error(traceback.print_tb(exc.__traceback__))
